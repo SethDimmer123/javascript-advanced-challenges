b775f281af6b9e84d95db379cbe2139f416b30ed
Seth DImmer <sethdimmer1@gmail.com>
2022-08-16 10:56:02 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Question 1. Show rating
+
+// Given a rating, display a star(*) for each full rating and a full stop(.) for each half rating.
+
+showRating(3) -->"* * *"
+
+showRating(4.5) -->"* * * * ."
+
+showRating(0.5) -->"."
