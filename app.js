1d4daf6e052a4182e945ab1b263fd2ac804baebc
Seth DImmer <sethdimmer1@gmail.com>
2022-11-27 15:35:36 -0500

    initial commit

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ function showRating(rating) {
     let ratings = ""
     for(let i = 0; i < rating; ++i) {
         ratings = ratings + "*"
-        if(i !== 3) {
+        if(i !== rating - 1) {
             ratings  = ratings + " "
         }
     }
     return ratings;
 }
 
-console.log(showRating(4));
+console.log(showRating(2));
 
 
 
